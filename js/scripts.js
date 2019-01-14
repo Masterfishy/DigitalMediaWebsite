1e5f66025e5b7bc2b7d96d6c58b31986aa0a5813
zachtl13 <zachtl13@vt.edu>
2019-01-14 17:44:06 -0500

    Poor navigation pannel

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Website scripts
+
+// Handle openning and closing navigation window in mobile view.~~~~~~~~~~~~~~
+// Open the menu.
+function openMenu() {
+    document.getElementById("menus").style.width="100%";
+}
+
+// Close the menu.
+function closeMenu() {
+    document.getElementById("menus").style.width="0%";
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
